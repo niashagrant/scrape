cb2a1bddbd62a6d7151902efc10470cd409862fb
Niasha Abaglo-Grant <niashagrant11@gmail.com>
2020-06-16 02:27:13 -0700

    still trouble shooting but was able to delete saved articles from the database only need to save the notes and be able to delete them

--- public/app.js
+++ public/app.js
@@ -44,8 +44,12 @@ $(document).on("click", ".seeNotes", function () {
   $(this).parent().parent().parent().find(".hiddenNotes").toggle();
 });
 
+$(document).on("click", ".viewNotes", function () {
+  $(this).parent().parent().parent().find(".showNotes").toggle();
+});
+
 $(document).on("click", ".deleteArticle", function () {
-  var articleID = $(this).data("articleID");
+  var articleID = $(this).attr("data-articleID");
   console.log(articleID);
   $.ajax({
     method: "DELETE",
@@ -54,4 +58,59 @@ $(document).on("click", ".deleteArticle", function () {
     console.log(data);
     window.location = "/saved";
   });
+});
+
+// Whenever someone clicks a p tag
+$(document).on("click", ".viewNotes", function () {
+  // Empty the notes from the note section
+  $("#notes").empty();
+  // Save the id from the p tag
+  var articleID = $(this)
+    .parent()
+    .parent()
+    .siblings(".media")
+    .find(".deleteArticle")
+    .attr("data-articleID");
+  console.log(articleID);
+
+  // Now make an ajax call for the Article
+  $.ajax({
+    method: "GET",
+    url: "/articles/" + articleID,
+  })
+    // With that done, add the note information to the page
+    .then(function (data) {
+      console.log(data);
+
+      // If there's a note in the article
+      if (data.note) {
+        // Place the body of the note in the body textarea
+        $("#bodyinput").val(data.note.body);
+      }
+    });
+});
+
+$(document).on("click", ".saveNote", function () {
+  console.log("saved note");
+  var articleID = $(this)
+    .parent()
+    .parent()
+    .siblings(".media")
+    .find(".deleteArticle")
+    .attr("data-articleID");
+  console.log(articleID);
+  $.ajax({
+    method: "POST",
+    url: "/articles/" + articleID,
+    data: {
+      // Value taken from note textarea
+      body: $(".bodyinput").val(),
+    },
+  }).then(function (data) {
+    // Log the response
+    console.log(data);
+    // Empty the notes section
+    $("#notes").empty();
+  });
+  $(".bodyinput").val("");
 });
--- server.js
+++ server.js
@@ -85,6 +85,23 @@ app.post("/saved", function (req, res) {
     });
 });
 
+app.get("/articles/:id", function (req, res) {
+  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  db.Article.findOne({ _id: req.params.id })
+    // ..and populate all of the notes associated with it
+    .populate("Note")
+    .then(function (dbArticle) {
+      // If we were able to successfully find an Article with the given id, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function (err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+
+  console.log(_id);
+});
+
 app.delete("/deleteArticle/:id", function (req, res) {
   var articleID = req.params.id;
   db.Article.remove({ _id: articleID })
@@ -93,6 +110,24 @@ app.delete("/deleteArticle/:id", function (req, res) {
     })
     .catch(function (err) {
       console.log(err);
+    });
+});
+
+// Route for saving/updating an Article's associated Note
+app.post("/articles/:id", function (req, res) {
+  db.Note.create(req.body)
+    .then(function (dbNote) {
+      return db.Article.findOneAndUpdate(
+        { _id: req.params.id },
+        { note: dbNote._id },
+        { new: true }
+      );
+    })
+    .then(function (dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function (err) {
+      res.json(err);
     });
 });
 
